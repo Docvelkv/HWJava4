f5e6107ec050392360d47fdfcad82cfff4c10ed0
Docvel <docvel@yandex.ru>
2023-04-08 02:53:47 +0300

    Initial commit

---
+++ src/main/java/Methods.java
@@ -0,0 +1,17 @@
+import java.util.Deque;
+
+public class Methods {
+    /**
+     * Создание "двусторонней очереди"
+     * Просмотр в обратном порядке
+     * Удаление последнего введённого элемента
+     *
+     * @param print   строка для просмотра коллекции
+     * @param delLast строка для удаления последнего введённого значения
+     * @param stop    строка для выхода из метода
+     * @return ArrayDeque<>()
+     */
+    public static Deque<String> workOfDeque(String print, String delLast, String stop) {
+
+    }
+}
